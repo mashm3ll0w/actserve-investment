a3cfa6972d9028f173e1e572c49633045845498b
mashm3ll0w <charlswall@live.com>
2024-09-20 13:08:05 +0300

    fix the field naming on the models

--- investments/models.py
+++ investments/models.py
@@ -34,15 +34,15 @@ class UserAccount(models.Model):
         (POST_ONLY, 'Post only')
     ]
 
-    user_account = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, related_name="user_account")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, related_name="user_account")
     investment_account = models.ForeignKey(InvestmentAccount, on_delete=models.CASCADE, null=False, blank=False, related_name='investment_account')
     permissions = models.CharField(max_length=16, choices=PERMISSION_CHOICES, null=False, blank=False)
 
     class Meta:
-        unique_together = ('user_account', 'investment_account')
+        unique_together = ('user', 'investment_account')
 
     def __str__(self):
-        return f'{self.user_account.username}'
+        return f'{self.user.username}'
 
 
 class Transaction(models.Model):
@@ -57,7 +57,7 @@ class Transaction(models.Model):
     ]
 
     investment_account = models.ForeignKey(InvestmentAccount, related_name='investment_transactions', on_delete=models.CASCADE, null=False, blank=False)
-    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE, null=False, blank=False, related_name="user_transaction")
+    user_account = models.ForeignKey(UserAccount, on_delete=models.CASCADE, null=False, blank=False, related_name="user_transaction")
     amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     transaction_date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     transaction_type = models.CharField(max_length=16, choices=TRANSACTION_TYPE_CHOICES, null=False, blank=False)
